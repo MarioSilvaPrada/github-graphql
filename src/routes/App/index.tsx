8e4f86c972c97c7592fed6f361f510e3a458fcab
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-05-04 23:39:49 +0100

    update

--- src/routes/App/index.tsx
+++ src/routes/App/index.tsx
@@ -1,10 +1,26 @@
 import React, { FC } from 'react';
 import * as S from './style';
 
-const App: FC = () => (
-  <S.Container>
-    <h1>Home</h1>
-  </S.Container>
-);
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const USERS_QUERY = gql`
+  {
+    viewer {
+      login
+    }
+  }
+`;
+
+const App: FC = () => {
+  const { loading, error, data } = useQuery(USERS_QUERY);
+  console.log(loading, error, data);
+
+  return (
+    <S.Container>
+      <h1>Home</h1>
+    </S.Container>
+  );
+};
 
 export default App;
